baa15be547e681d708090c1c46ff45634c79c359
Satyasheel Singh <satyasheel2004@gmail.com>
2026-06-25 10:31:47 +0530

    refactor: extract firstSubmatch helper for token extraction

--- internal/portal/magic.go
+++ internal/portal/magic.go
@@ -8,37 +8,23 @@ var formRe = regexp.MustCompile(`name="magic"[[:space:]]+value="?([a-f0-9]+)`)
 var keepaliveRe = regexp.MustCompile(`keepalive\?([a-f0-9]+)`)
 
 func magicFromRedirect(redirectURL string) (string, bool) {
-	capture := redirectRe.FindStringSubmatch(redirectURL)
-
-	if capture == nil {
-		return "", false
-	}
-
-	return capture[1], true
+	return firstSubmatch(redirectRe, redirectURL)
 }
 
 func magicFromBody(body string) (string, bool) {
-	capture := bodyRe.FindStringSubmatch(body)
-
-	if capture == nil {
-		return "", false
-	}
-
-	return capture[1], true
+	return firstSubmatch(bodyRe, body)
 }
 
 func magicFromForm(html string) (string, bool) {
-	capture := formRe.FindStringSubmatch(html)
-
-	if capture == nil {
-		return "", false
-	}
-
-	return capture[1], true
+	return firstSubmatch(formRe, html)
 }
 
 func keepaliveFromBody(body string) (string, bool) {
-	capture := keepaliveRe.FindStringSubmatch(body)
+	return firstSubmatch(keepaliveRe, body)
+}
+
+func firstSubmatch(re *regexp.Regexp, s string) (string, bool) {
+	capture := re.FindStringSubmatch(s)
 
 	if capture == nil {
 		return "", false
